69f70ba8df5d0ad7c4ed93f27a54fcb8de99c5aa
Matt Thomas <matt@betweenbrain.com>
2014-01-16 16:35:48 -0500

    Implements a bit of dynamic functionality

--- map-apis/google/index.php
+++ map-apis/google/index.php
@@ -1,3 +1,22 @@
+<?php
+// create curl resource
+$ch = curl_init();
+
+// set url
+curl_setopt($ch, CURLOPT_URL, "http://maps.googleapis.com/maps/api/geocode/json?address=".urlencode($_GET['address'])."+06791&sensor=true");
+
+//return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+// $output contains the output string
+$output = json_decode(curl_exec($ch));
+
+// close curl resource to free up system resources
+curl_close($ch);
+
+$lat = $output->results[0]->geometry->location->lat;
+$lng = $output->results[0]->geometry->location->lng;
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,7 +27,11 @@
 		html, body, #map-canvas {
 			height  : 100%;
 			margin  : 0px;
-			padding : 0px
+			padding : 0px;
+		}
+		form {
+			position: absolute;
+			z-index: 1;
 		}
 	</style>
 	<script src="https://maps.googleapis.com/maps/api/js?v=3&sensor=false"></script>
@@ -17,18 +40,18 @@
 		function initialize() {
 			var mapOptions = {
 				zoom  : 16,
-				center: new google.maps.LatLng(40.782873, -73.959588)
+				center: new google.maps.LatLng(<?php echo $lat ?>,<?php echo $lng ?>)
 			};
 
 			map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
 			var contentString = '<div id="content">' +
-					'<h1>Guggenheim Museum <br/><small><small>(custom info window)</small></small></h1>' +
-					'<img src="http://media.guggenheim.org/content/New_York/about_us/au_home_690h.jpg" alt="The Solomon R. Guggenheim Museum" title="View of Solomon R. Guggenheim Museum exterior">' +
-					'<div id="bodyContent">' +
-					'<p>An internationally renowned art museum and one of the most significant architectural icons of the 20th century, the Guggenheim Museum is at once a vital cultural center, an educational institution, and the heart of an international network of museums. Visitors can experience special exhibitions of modern and contemporary art, lectures by artists and critics, performances and film screenings, classes for teens and adults, and daily tours of the galleries led by museum educators. Founded on a collection of early modern masterpieces, the Guggenheim Museum today is an ever-growing institution devoted to the art of the 20th century and beyond.</p>' +
-					'</div>' +
-					'</div>';
+				'<h1>Guggenheim Museum <br/><small><small>(custom info window)</small></small></h1>' +
+				'<img src="http://media.guggenheim.org/content/New_York/about_us/au_home_690h.jpg" alt="The Solomon R. Guggenheim Museum" title="View of Solomon R. Guggenheim Museum exterior">' +
+				'<div id="bodyContent">' +
+				'<p>An internationally renowned art museum and one of the most significant architectural icons of the 20th century, the Guggenheim Museum is at once a vital cultural center, an educational institution, and the heart of an international network of museums. Visitors can experience special exhibitions of modern and contemporary art, lectures by artists and critics, performances and film screenings, classes for teens and adults, and daily tours of the galleries led by museum educators. Founded on a collection of early modern masterpieces, the Guggenheim Museum today is an ever-growing institution devoted to the art of the 20th century and beyond.</p>' +
+				'</div>' +
+				'</div>';
 
 			var infowindow = new google.maps.InfoWindow({
 				content: contentString,
@@ -99,7 +122,10 @@
 	</script>
 </head>
 <body>
+<form action="" method="get">
+	<label for="address">Enter your address</label>
+	<input type="text" name="address" size="32" />
+</form>
 <div id="map-canvas"></div>
 </body>
 </html>
-
