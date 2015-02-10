61acdd2aef11c57035a868bdece085f55f56545f
Matt Thomas <matt@betweenbrain.com>
2015-02-10 13:57:11 -0500

    Multiple carousel swiper example

--- js/swiper-carousel/looped/index.php
+++ js/swiper-carousel/looped/index.php
@@ -7,7 +7,141 @@
 <body>
 <?php include dirname(dirname(__FILE__)) . '/images/images.php'; ?>
 <div class="wrapper">
-	<div class="slideshow">
+	<div class="slideshow1">
+		<div class="slider">
+			<?php foreach ($images as $image) : ?>
+				<div class="slide">
+					<img src="<?php echo $image->src ?>" />
+					<p><?php echo $image->text ?></p>
+				</div>
+			<?php endforeach ?>
+		</div>
+		<div class="ImageSliderControls">
+			<a id="property-details-swiper_prev" href="" class="ImageSliderPrev ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_left"></i>
+				<span>prev</span>
+			</a>
+			<a id="property-details-swiper_next" href="" class="ImageSliderNext ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_right"></i>
+				<span>next</span>
+			</a>
+		</div>
+	</div>
+</div>
+<div class="wrapper">
+	<div class="slideshow2">
+		<div class="slider">
+			<?php foreach ($images as $image) : ?>
+				<div class="slide">
+					<img src="<?php echo $image->src ?>" />
+					<p><?php echo $image->text ?></p>
+				</div>
+			<?php endforeach ?>
+		</div>
+		<div class="ImageSliderControls">
+			<a id="property-details-swiper_prev" href="" class="ImageSliderPrev ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_left"></i>
+				<span>prev</span>
+			</a>
+			<a id="property-details-swiper_next" href="" class="ImageSliderNext ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_right"></i>
+				<span>next</span>
+			</a>
+		</div>
+	</div>
+</div>
+<div class="wrapper">
+	<div class="slideshow3">
+		<div class="slider">
+			<?php foreach ($images as $image) : ?>
+				<div class="slide">
+					<img src="<?php echo $image->src ?>" />
+					<p><?php echo $image->text ?></p>
+				</div>
+			<?php endforeach ?>
+		</div>
+		<div class="ImageSliderControls">
+			<a id="property-details-swiper_prev" href="" class="ImageSliderPrev ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_left"></i>
+				<span>prev</span>
+			</a>
+			<a id="property-details-swiper_next" href="" class="ImageSliderNext ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_right"></i>
+				<span>next</span>
+			</a>
+		</div>
+	</div>
+</div>
+<div class="wrapper">
+	<div class="slideshow4">
+		<div class="slider">
+			<?php foreach ($images as $image) : ?>
+				<div class="slide">
+					<img src="<?php echo $image->src ?>" />
+					<p><?php echo $image->text ?></p>
+				</div>
+			<?php endforeach ?>
+		</div>
+		<div class="ImageSliderControls">
+			<a id="property-details-swiper_prev" href="" class="ImageSliderPrev ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_left"></i>
+				<span>prev</span>
+			</a>
+			<a id="property-details-swiper_next" href="" class="ImageSliderNext ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_right"></i>
+				<span>next</span>
+			</a>
+		</div>
+	</div>
+</div>
+
+<div class="wrapper">
+	<div class="slideshow5">
+		<div class="slider">
+			<?php foreach ($images as $image) : ?>
+				<div class="slide">
+					<img src="<?php echo $image->src ?>" />
+					<p><?php echo $image->text ?></p>
+				</div>
+			<?php endforeach ?>
+		</div>
+		<div class="ImageSliderControls">
+			<a id="property-details-swiper_prev" href="" class="ImageSliderPrev ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_left"></i>
+				<span>prev</span>
+			</a>
+			<a id="property-details-swiper_next" href="" class="ImageSliderNext ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_right"></i>
+				<span>next</span>
+			</a>
+		</div>
+	</div>
+</div>
+<div class="wrapper">
+	<div class="slideshow6">
+		<div class="slider">
+			<?php foreach ($images as $image) : ?>
+				<div class="slide">
+					<img src="<?php echo $image->src ?>" />
+					<p><?php echo $image->text ?></p>
+				</div>
+			<?php endforeach ?>
+		</div>
+		<div class="ImageSliderControls">
+			<a id="property-details-swiper_prev" href="" class="ImageSliderPrev ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_left"></i>
+				<span>prev</span>
+			</a>
+			<a id="property-details-swiper_next" href="" class="ImageSliderNext ImageSliderPrevNext">
+				<i class="icon-tk-lrg_arrow_right"></i>
+				<span>next</span>
+			</a>
+		</div>
+	</div>
+</div>
+
+<div class="wrapper">
+	<div class="slideshow7">
 		<div class="slider">
 			<?php foreach ($images as $image) : ?>
 				<div class="slide">
@@ -32,59 +166,57 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="../js/idangerous.swiper.js"></script>
 <script>
-	(function ($) {
-		$(document).ready(function () {
-			var mySwiper = $('.slideshow').swiper({
-				calculateHeight: true,
-				loop           : true,
-				slideClass     : "slide",
-				slidesPerView  : 4,
-				slidesPerGroup : 4,
-				wrapperClass   : "slider"
-			});
+	function initSlider(slider) {
+		(function ($) {
+			$(slider).each(function () {
+				var mySwiper = $(this).swiper({
+					calculateHeight: true,
+					loop           : true,
+					slideClass     : "slide",
+					slidesPerView  : 4,
+					slidesPerGroup : 4,
+					wrapperClass   : "slider"
+				});
 
-			$('.ImageSliderPrev').click(function (e) {
-				e.preventDefault();
-				mySwiper.swipePrev();
-			});
+				$(this).find('.ImageSliderPrev').click(function (e) {
+					e.preventDefault();
+					mySwiper.swipePrev();
+				});
 
-			$('.ImageSliderNext').click(function (e) {
-				e.preventDefault();
-				mySwiper.swipeNext();
-			});
+				$(this).find('.ImageSliderNext').click(function (e) {
+					e.preventDefault();
+					mySwiper.swipeNext();
+				});
 
-			reInitSlider(mySwiper);
-
-			$(window).smartresize(function () {
 				reInitSlider(mySwiper);
+
+				$(window).smartresize(function () {
+					reInitSlider(mySwiper);
+				});
 			});
+		})(jQuery);
+	}
 
-			function reInitSlider(mySwiper) {
+	function reInitSlider(mySwiper) {
+		mySwiper.params.slidesPerView = 4;
+		mySwiper.params.slidesPerGroup = 4;
 
-				mySwiper.params.slidesPerView = 4;
-				mySwiper.params.slidesPerGroup = 4;
+		if ($(window).width() < 1200) {
+			mySwiper.params.slidesPerView = 3;
+			mySwiper.params.slidesPerGroup = 3;
+		}
 
-				if ($(window).width() < 1200) {
-					mySwiper.params.slidesPerView = 3;
-					mySwiper.params.slidesPerGroup = 3;
-				}
+		if ($(window).width() < 992) {
+			mySwiper.params.slidesPerView = 2;
+			mySwiper.params.slidesPerGroup = 2;
+		}
 
-				if ($(window).width() < 992) {
-					mySwiper.params.slidesPerView = 2;
-					mySwiper.params.slidesPerGroup = 2;
-				}
-
-				if ($(window).width() < 768) {
-					mySwiper.params.slidesPerView = 1;
-					mySwiper.params.slidesPerGroup = 1;
-				}
-
-				mySwiper.reInit();
-			}
-
-
-		});
-	}(jQuery));
+		if ($(window).width() < 768) {
+			mySwiper.params.slidesPerView = 1;
+			mySwiper.params.slidesPerGroup = 1;
+		}
+		mySwiper.reInit();
+	}
 
 	// Debounced Resize() jQuery Plugin
 	// http://www.paulirish.com/2009/throttled-smartresize-jquery-event-handler/
@@ -118,7 +250,19 @@
 		};
 
 	})(jQuery, 'smartresize');
-
+</script>
+<script>
+	(function ($) {
+		$(document).ready(function () {
+			initSlider('.slideshow1');
+			initSlider('.slideshow2');
+			initSlider('.slideshow3');
+			initSlider('.slideshow4');
+			initSlider('.slideshow5');
+			initSlider('.slideshow6');
+			initSlider('.slideshow7');
+		});
+	})(jQuery);
 </script>
 </body>
 </html>
