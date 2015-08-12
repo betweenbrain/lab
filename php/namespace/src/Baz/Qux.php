945fc747c0228b09ed4e66c768807da4595d2240
Matt Thomas <matt@betweenbrain.com>
2015-08-12 15:55:10 -0400

    Implement project-specific autoloading

--- php/namespace/src/Baz/Qux.php
+++ php/namespace/src/Baz/Qux.php
@@ -1,12 +1,21 @@
 <?php
-require 'autoload.php';
+
 /**
- * File       index.php
- * Created    8/12/15 2:31 PM
+ * File       Test.php
+ * Created    8/12/15 3:17 PM
  * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
  * Support    https://github.com/betweenbrain/
  * Copyright  Copyright (C) 2015 betweenbrain llc. All Rights Reserved.
  * License    GNU GPL v2 or later
  */
 
-new \Foo\Bar\Baz\Qux;
+namespace Foo\Bar\Baz;
+
+class Qux
+{
+
+	public function __construct()
+	{
+		echo __CLASS__ . '<br/>';
+	}
+}
